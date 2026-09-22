2c71c0fb06ab22fdc19c14d6cd3a4e5eb6b2b9ff
oppie549 <omprakashbandi583@gmail.com>
2026-09-22 21:09:09 +0530

    created repository

--- src/main/java/com/cravedash/paymentservice/repository/PaymentRepository.java
+++ src/main/java/com/cravedash/paymentservice/repository/PaymentRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
 public interface PaymentRepository
         extends JpaRepository<Payment, Long> {
+
 
     Optional<Payment> findByOrderId(Long orderId);
 }
